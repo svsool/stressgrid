ab5065358b040f63c79f6e133d7c71e2558843d7
Svyat Sobol <svyat.gh@gmail.com>
2025-12-16 11:43:54 +0200

    Update config.exs

--- generator/config/config.exs
+++ generator/config/config.exs
@@ -14,7 +14,7 @@ import_config "#{Mix.env()}.exs"
 
 scripts_path = System.get_env("SCRIPTS_PATH") || "../scripts"
 
-custom_scripts_config = Path.expand(Path.join(scripts_path, "config.exs"), __DIR__)
+custom_scripts_config = Path.expand(Path.join([scripts_path, "config", "config.exs"]), __DIR__)
 
 if File.exists?(custom_scripts_config) do
   import_config custom_scripts_config
